9713f8d6c28af6a57a6d00ec8c816ffff1cd35c0
hablasharif <138958393+hablasharif@users.noreply.github.com>
2024-07-16 20:29:14 +0600

    Update 5-box-tq5-GUI-For-Dictionary.py

--- 5-box-tq5-GUI-For-Dictionary.py
+++ 5-box-tq5-GUI-For-Dictionary.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QLabel
-from PyQt5 import QtCore  # Import QtCore module for alignment constants
+from PyQt5 import QtCore, QtGui
 
 class InputBoxes(QWidget):
     def __init__(self):
@@ -18,29 +18,47 @@ class InputBoxes(QWidget):
         self.input_box4 = QLineEdit(self)
         self.input_box5 = QLineEdit(self)
 
+        # Set font for Bengali support (using Kalpurush)
+        font_bengali = QtGui.QFont()
+        font_bengali.setFamily("Kalpurush")
+        font_bengali.setPointSize(12)  # Adjust font size as needed
+
+        self.input_box1.setFont(font_bengali)
+        self.input_box2.setFont(font_bengali)
+        self.input_box3.setFont(font_bengali)
+        self.input_box4.setFont(font_bengali)
+        self.input_box5.setFont(font_bengali)
+
         # Labels for the input boxes
-        self.input_label1 = QLabel('Input 1:', self)
-        self.input_label2 = QLabel('Input 2:', self)
-        self.input_label3 = QLabel('Input 3:', self)
-        self.input_label4 = QLabel('Input 4:', self)
-        self.input_label5 = QLabel('Input 5:', self)
+        self.input_label1 = QLabel('ইনপুট 1:', self)
+        self.input_label2 = QLabel('ইনপুট 2:', self)
+        self.input_label3 = QLabel('ইনপুট 3:', self)
+        self.input_label4 = QLabel('ইনপুট 4:', self)
+        self.input_label5 = QLabel('ইনপুট 5:', self)
+
+        # Set labels to use the same Bengali font
+        self.input_label1.setFont(font_bengali)
+        self.input_label2.setFont(font_bengali)
+        self.input_label3.setFont(font_bengali)
+        self.input_label4.setFont(font_bengali)
+        self.input_label5.setFont(font_bengali)
 
         # Set fixed size for box3, box4, box5
         box3_width = 300
-        box3_height = 300
-        box4_width = 450
-        box4_height = 300
-        box5_width = 450
-        box5_height = 300
+        box3_height = 300  # Adjusted height to prevent breaking
+        box4_width = 300
+        box4_height = 300  # Adjusted height to prevent breaking
+        box5_width = 300
+        box5_height = 300  # Adjusted height to prevent breaking
 
         self.input_box3.setFixedSize(box3_width, box3_height)
         self.input_box4.setFixedSize(box4_width, box4_height)
         self.input_box5.setFixedSize(box5_width, box5_height)
 
         # Set initial sizes for box1 and box2
-        bottom_box1_width = 770  # Decreased width for input_box1
-        bottom_box2_width = 500  # Increased width for input_box2
-        bottom_box_height = 400  # Adjust this height as needed
+        bottom_box1_width = 700  # Decreased width for input_box1
+        bottom_box2_width = 500  # Decreased width for input_box2
+        bottom_box_height = 400  # Adjusted height to prevent breaking
 
         self.input_box1.setFixedWidth(bottom_box1_width)
         self.input_box1.setMinimumHeight(bottom_box_height)
